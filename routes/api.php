e6aabea66285276871df8ed06b2f88ae79856963
Koong Hiap <koong.hiap@concise.co.id>
2025-08-27 16:17:27 +0700

    feat: 🎸 model migration

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/register');
+Route::post('/login');
+Route::post('/refresh');
+Route::middleware('auth:sanctum')->group(function(){
+    Route::prefix('posts')->group(function(){
+        Route::get("/");
+        Route::post("/");
+        Route::get("/{post}");
+        Route::put("/{post}");
+        Route::delete("/{post}");
+        Route::get("/{post}/bookmarks");
+    });
+
+    Route::prefix('users')->group(function(){
+        Route::get('/');
+        Route::get('/{user}');
+        Route::get("/{user}/posts");
+        Route::put("/{user}");
+    });
 });
